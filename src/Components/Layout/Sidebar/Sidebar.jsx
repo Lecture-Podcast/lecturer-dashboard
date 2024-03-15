9653f91bcffda23b6dc635d904622f1c84bd396b
ChikenduHillary <chikenduhillchiagoziemu@gmail.com>
2024-03-15 23:11:29 +0100

    created the library ui

--- src/Components/Layout/Sidebar/Sidebar.jsx
+++ src/Components/Layout/Sidebar/Sidebar.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import logo from '../../../Assets/images/logo.png'
-import "./Sidebar.css"
+import React from "react";
+import logo from "../../../Assets/images/logo.png";
+import "./Sidebar.css";
 import { HiOutlineChartPie } from "react-icons/hi2";
 import { FiFolder } from "react-icons/fi";
 import { RiAddCircleLine } from "react-icons/ri";
@@ -9,64 +9,115 @@ import { FiSettings } from "react-icons/fi";
 import { TbBriefcase } from "react-icons/tb";
 import { PiSignOutLight } from "react-icons/pi";
 import { LiaMoneyBillWaveSolid } from "react-icons/lia";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 // import { useState } from 'react';
 const Sidebar = () => {
-    const [value, setValue] = React.useState(0);
-    const  handleChange = (event) => {
-        setValue(event)
-    }
-    return ( 
-        <div className="sidebar">
-            <div className="sidebar-logo">
-                <img src={logo}></img> 
-            </div>
-            <div className="sidebar-nav">
-                <nav>
-                    <ul>
-                        <Link to='/'>
-                            <li className={(value===0)?'active':''} onClick={()=>{handleChange(0)}}>
-                                <span><HiOutlineChartPie/></span>
-                                Dashboard
-                            </li>
-                        </Link>
-                        <li className={(value===1)?'active':''} onClick={()=>{handleChange(1)}}>
-                            <span><FiFolder/></span>
-                            Content Library
-                        </li>
-                        <Link to="/create">
-                            <li className={(value===2)?'active':''} onClick={()=>{handleChange(2)}}>
-                                <span><RiAddCircleLine/></span>
-                                Create
-                            </li>
-                        </Link>
-                        <li className={(value===3)?'active':''} onClick={()=>{handleChange(3)}}>
-                            <span><TbMessage/></span>
-                            Messages
-                        </li>
-                        <Link to="/settings">
-                            <li className={(value===4)?'active':''} onClick={()=>{handleChange(4)}}>
-                                <span><FiSettings/></span>
-                                Settings
-                            </li>
-                        </Link>
-                        <li className={(value===5)?'active':''} onClick={()=>{handleChange(5)}}>
-                            <span><TbBriefcase/></span>
-                            Supports & Help
-                        </li>
-                        <li className={(value===6)?'active':''} onClick={()=>{handleChange(6)}}>
-                            <span><LiaMoneyBillWaveSolid/></span>
-                            Premimum
-                        </li>
-                    </ul>
-                </nav>
-                <div className="log-out">
-                    <span><PiSignOutLight/></span>
-                    <p>Log Out</p>
-                </div>
-            </div>
+  const [value, setValue] = React.useState(0);
+  const handleChange = (event) => {
+    setValue(event);
+  };
+  return (
+    <div className="sidebar">
+      <div className="sidebar-logo">
+        <img src={logo}></img>
+      </div>
+      <div className="sidebar-nav">
+        <nav>
+          <ul>
+            <Link to="/">
+              <li
+                className={value === 0 ? "active" : ""}
+                onClick={() => {
+                  handleChange(0);
+                }}
+              >
+                <span>
+                  <HiOutlineChartPie />
+                </span>
+                Dashboard
+              </li>
+            </Link>
+            <li
+              className={value === 1 ? "active" : ""}
+              onClick={() => {
+                handleChange(1);
+              }}
+            >
+              <span>
+                <FiFolder />
+              </span>
+              Content Library
+            </li>
+            <Link to="/create">
+              <li
+                className={value === 2 ? "active" : ""}
+                onClick={() => {
+                  handleChange(2);
+                }}
+              >
+                <span>
+                  <RiAddCircleLine />
+                </span>
+                Create
+              </li>
+            </Link>
+            <li
+              className={value === 3 ? "active" : ""}
+              onClick={() => {
+                handleChange(3);
+              }}
+            >
+              <span>
+                <TbMessage />
+              </span>
+              Messages
+            </li>
+            <Link to="/settings">
+              <li
+                className={value === 4 ? "active" : ""}
+                onClick={() => {
+                  handleChange(4);
+                }}
+              >
+                <span>
+                  <FiSettings />
+                </span>
+                Settings
+              </li>
+            </Link>
+            <li
+              className={value === 5 ? "active" : ""}
+              onClick={() => {
+                handleChange(5);
+              }}
+            >
+              <span>
+                <TbBriefcase />
+              </span>
+              Supports & Help
+            </li>
+            <li
+              className={value === 6 ? "active" : ""}
+              onClick={() => {
+                handleChange(6);
+              }}
+            >
+              <span>
+                <LiaMoneyBillWaveSolid />
+              </span>
+              Premimum
+            </li>
+          </ul>
+        </nav>
+        <div className="log-out">
+          <span>
+            <PiSignOutLight />
+          </span>
+          <p>Log Out</p>
         </div>
-    );
-}
- 
+      </div>
+    </div>
+  );
+};
+
 export default Sidebar;
--- src/routes.js
+++ src/routes.js
@@ -1,35 +1,36 @@
-import React from 'react'
+import React from "react";
 import { useRoutes } from "react-router-dom";
 import Layout from "./Components/Layout/Layout";
 import Home from "./Pages/Home/Home";
-import Settings from './Pages/Settings/Settings';
-import Create from './Pages/Create/Create';
-import Uplaod from './Pages/Create/upload';
-
+import Settings from "./Pages/Settings/Settings";
+import Create from "./Pages/Create/Create";
+import Uplaod from "./Pages/Create/upload";
+import Content from "./Pages/create-content/crearte-content";
+import ContentLibrary from "./Pages/content-library";
 
 export default function Router() {
-    const routes = useRoutes([
+  const routes = useRoutes([
+    // {
+    //   path: '/',
+    //   element: <Login/>, // Renders LoginPage component when the root path is accessed
+    // },
+    // {
+    //   path: '/signup',
+    //   element: <Signup/>, // Renders LoginPage component when the root path is accessed
+    // },
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        { index: true, element: <Home /> },
+        { path: "settings", element: <Settings /> },
+        { path: "create", element: <Create /> },
+        { path: "/create/upload", element: <Uplaod /> },
+        { path: "/create-content", element: <Content /> },
+        { path: "/create-library", element: <ContentLibrary /> },
+      ],
+    },
+  ]);
 
-        // {
-        //   path: '/',
-        //   element: <Login/>, // Renders LoginPage component when the root path is accessed
-        // },
-        // {
-        //   path: '/signup',
-        //   element: <Signup/>, // Renders LoginPage component when the root path is accessed
-        // },
-        {
-            path: '/',
-            element: <Layout/>,
-            children: [
-              { index: true, element: <Home/> },
-              { path: 'settings', element: <Settings/> },
-              { path: 'create', element: <Create/> },
-              { path: '/create/upload', element: <Uplaod/> },
-            ],
-        },
-    ]);
-    
-  
-    return routes;
-  }
+  return routes;
+}

ef6530d1a5b49b5a17fde474ca8642f27327c235
Precious1324 <aleriwaprecious70@gmail.com>
2024-03-15 16:56:28 +0100

    New Update:(

--- src/Components/Chart/Chart.jsx
+++ src/Components/Chart/Chart.jsx
@@ -1,30 +1,16 @@
 import React from 'react';
 import './Chart.css'
-import { Line } from 'react-chartjs-2';
-import {
-    Chart as ChartJS,
-    LineElement,
-    PointElement,
-    CategoryScale,
-    LinearScale
-}  from 'chart.js';
-import Chart from 'chart.js/auto';
+import Chart from "react-apexcharts"
 import { useRef } from 'react';
 
 
-ChartJS.register(
-    LineElement,
-    CategoryScale,
-    LinearScale,
-    PointElement
-)
 const Graph = () => {
     const chartRef = useRef(null);
     
     const initialBackgroundColor = 'rgba(52, 202, 165, 0.10)';
     const chartData = {
         labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', "12"],
-        datasets: [{
+        data: [{
             label:"Documents",
             data:[200, 300, 400, 350, 500, 600, 550, 400, 500, 700, 750, 850],
             borderColor: "#F56630",
@@ -79,11 +65,81 @@ const Graph = () => {
               display: true, // Remove legend
             },
           },
+    };
+    const data = {
+        series: [
+          {
+            name: "High - 2013",
+            data: [28, 29, 33, 36, 32, 32, 33],
+          },
+          {
+            name: "Low - 2013",
+            data: [12, 11, 14, 18, 17, 13, 13],
+          },
+        ],
+        options: {
+          chart: {
+            height: 350,
+            type: "line",
+            dropShadow: {
+              enabled: true,
+              color: "#000",
+              top: 18,
+              left: 7,
+              blur: 10,
+              opacity: 0.2,
+            },
+            toolbar: {
+              show: false,
+            },
+          },
+          colors: ["#77B6EA", "#545454"],
+          dataLabels: {
+            enabled: true,
+          },
+          stroke: {
+            curve: "smooth",
+          },
+          title: {
+            text: "Average High & Low Temperature",
+            align: "left",
+          },
+          grid: {
+            borderColor: "#e7e7e7",
+            row: {
+              colors: ["#f3f3f3", "transparent"], // takes an array which will be repeated on columns
+              opacity: 0.5,
+            },
+          },
+          markers: {
+            size: 1,
+          },
+          xaxis: {
+            categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"],
+            title: {
+              text: "Month",
+            },
+          },
+          yaxis: {
+            title: {
+              text: "Temperature",
+            },
+            min: 5,
+            max: 40,
+          },
+          legend: {
+            position: "top",
+            horizontalAlign: "right",
+            floating: true,
+            offsetY: -25,
+            offsetX: -5,
+          },
+        },
       };
-
     return ( 
         <div className="chart" style={{height: "250px"}}>
-            <Line data={chartData} options={options}/>
+            <Chart series={data.series} options={data.options} type="line" height={250}/>
+            {/* <Line data={chartData} options={options}/> */}
         </div>
     );
 }

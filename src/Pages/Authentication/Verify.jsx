effd4f36bac45e448b8a4f5f97f57715e8502897
Precious1324 <aleriwaprecious70@gmail.com>
2024-04-08 02:11:32 +0100

    With Login and Sign-up

--- src/Pages/Authentication/Verify.jsx
+++ src/Pages/Authentication/Verify.jsx
@@ -17,7 +17,7 @@ const Verify = ({loading, error, verifyaction}) => {
     const handleSubmit = (e) =>{
         e.preventDefault()
         verifyaction(tokenFromUrl, ()=>{
-            history.push("https://lecture-podcast-dahboard.netlify.app/")
+            history("https://lecture-podcast-dahboard.netlify.app/")
         })
     }
     return ( 
